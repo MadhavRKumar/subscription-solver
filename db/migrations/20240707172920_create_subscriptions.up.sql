f2eae6af18bf120a113ebbb19fc24b225741c489
MadhavRKumar <madhavrkumar@gmail.com>
2024-07-17 21:07:00 -0400

    add timestamps and auto update

--- db/migrations/20240707172920_create_subscriptions.up.sql
+++ db/migrations/20240707172920_create_subscriptions.up.sql
@@ -3,5 +3,20 @@ CREATE TABLE IF NOT EXISTS subscriptions(
     uuid UUID NOT NULL,
     name VARCHAR (50) NOT NULL,
     profile_limit int NOT NULL,
-    cost int NOT NULL
+    cost int NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL
 );
+
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_your_table_modtime
+    BEFORE UPDATE ON subscriptions
+    FOR EACH ROW EXECUTE FUNCTION update_modified_column();
