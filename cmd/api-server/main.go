241d0cc9d1c53a917ca9bd0162c2ed04a64d70f4
MadhavRKumar <madhavrkumar@gmail.com>
2024-09-02 15:59:31 -0400

    feat: create api and docker setup

---
+++ cmd/api-server/main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+
+	"github.com/MadhavRKumar/subscription-solver/internal/subscriptions"
+    "github.com/MadhavRKumar/subscription-solver/internal/middleware/cors"
+)
+
+type SubscriptionsHandler struct {
+	store subscriptionStore
+}
+
+type subscriptionStore interface {
+	Add(uuid string, subscription subscriptions.Subscription) (subscriptions.Subscription, error)
+	Get(uuid string) (subscriptions.Subscription, error)
+	List() ([]subscriptions.Subscription, error)
+	Update(uuid string, subscription subscriptions.Subscription) error
+	Remove(uuid string) error
+}
+
+func main() {
+    gin.SetMode(gin.ReleaseMode)
+
+	store := subscriptions.NewMemStore()
+	handler := NewSubscriptionsHandler(store)
+
+	router := gin.New()
+    router.Use(cors.Cors())
+    router.Use(gin.Logger())
+    router.Use(gin.Recovery())
+
+	router.GET("/subscriptions", handler.ListSubscription)
+	router.GET("/subscriptions/:id", handler.GetSubscription)
+	router.POST("/subscriptions", handler.CreateSubscription)
+	router.DELETE("/subscriptions/:id", handler.DeleteSubscription)
+	router.PATCH("/subscriptions/:id", handler.UpdateSubscription)
+
+	err := router.Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewSubscriptionsHandler(s subscriptionStore) *SubscriptionsHandler {
+	return &SubscriptionsHandler{
+		store: s,
+	}
+}
+
+func (h *SubscriptionsHandler) CreateSubscription(c *gin.Context) {
+	var newSubscription subscriptions.Subscription
+
+	if err := c.BindJSON(&newSubscription); err != nil {
+
+		if err := c.AbortWithError(http.StatusInternalServerError, err); err != nil {
+			return
+		}
+		return
+	}
+
+	uuid := uuid.New().String()
+
+	sub, err := h.store.Add(uuid, newSubscription)
+	if err != nil {
+		if err := c.AbortWithError(http.StatusInternalServerError, err); err != nil {
+			return
+		}
+
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, sub)
+}
+
+func (h *SubscriptionsHandler) ListSubscription(c *gin.Context) {
+	subscriptions, err := h.store.List()
+	if err != nil {
+		if err := c.AbortWithError(http.StatusInternalServerError, err); err != nil {
+			return
+		}
+
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, subscriptions)
+}
+
+func (h *SubscriptionsHandler) GetSubscription(c *gin.Context) {
+	uuid := c.Param("id")
+
+	sub, err := h.store.Get(uuid)
+	if err != nil {
+
+		if _, ok := err.(*subscriptions.NotFoundError); ok {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		if err := c.AbortWithError(http.StatusInternalServerError, err); err != nil {
+			return
+		}
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, sub)
+}
+
+func (h *SubscriptionsHandler) DeleteSubscription(c *gin.Context) {
+	uuid := c.Param("id")
+
+	err := h.store.Remove(uuid)
+	if err != nil {
+		if _, ok := err.(*subscriptions.NotFoundError); ok {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		if err := c.AbortWithError(http.StatusInternalServerError, err); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
+
+func (h *SubscriptionsHandler) UpdateSubscription(c *gin.Context) {
+    uuid := c.Param("id")
+
+    var updatedSubscription subscriptions.Subscription
+
+    if err := c.BindJSON(&updatedSubscription); err != nil {
+        if err := c.AbortWithError(http.StatusInternalServerError, err); err != nil {
+            return
+        }
+        return
+    }
+
+    err := h.store.Update(uuid, updatedSubscription)
+
+    if err != nil {
+        if _, ok := err.(*subscriptions.NotFoundError); ok {
+            c.AbortWithStatus(http.StatusNotFound)
+            return
+        }
+
+        if err := c.AbortWithError(http.StatusInternalServerError, err); err != nil {
+            return
+        }
+        return
+    }
+
+    c.Status(http.StatusNoContent)
+}
---
+++ internal/subscriptions/memstore.go
@@ -0,0 +1,99 @@
+package subscriptions
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type memStore struct {
+	conn *pgxpool.Pool
+}
+
+type NotFoundError struct {
+	message string
+}
+
+func (e *NotFoundError) Error() string {
+	return e.message
+}
+
+func NewMemStore() *memStore {
+	log.Println(os.Getenv("POSTGRES_URL"))
+	conn, err := pgxpool.New(context.Background(), os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		os.Exit(1)
+	}
+
+	return &memStore{
+		conn: conn,
+	}
+}
+
+func (m *memStore) Add(uuid string, subscription Subscription) (Subscription, error) {
+	_, err := m.conn.Exec(context.Background(), "INSERT INTO subscriptions (uuid, name, profile_limit, cost) VALUES ($1, $2, $3, $4)", uuid, subscription.Name, subscription.ProfileLimit, subscription.Cost)
+
+	subscription.UUID = uuid
+
+	return subscription, err
+}
+
+func (m *memStore) Get(uuid string) (Subscription, error) {
+	var sub Subscription
+	err := m.conn.QueryRow(context.Background(), "SELECT uuid, name, profile_limit, cost FROM subscriptions where uuid=$1 and deleted_at is NULL", uuid).Scan(&sub.UUID, &sub.Name, &sub.ProfileLimit, &sub.Cost)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return Subscription{}, &NotFoundError{"Subscription not found"}
+		}
+		return Subscription{}, err
+	}
+
+	return sub, nil
+}
+
+func (m *memStore) List() ([]Subscription, error) {
+	var subscriptions []Subscription
+
+	rows, err := m.conn.Query(context.Background(), "SELECT uuid, name, profile_limit, cost FROM subscriptions WHERE deleted_at is NULL ORDER BY created_at DESC")
+	if err != nil {
+		return subscriptions, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var sub Subscription
+		err := rows.Scan(&sub.UUID, &sub.Name, &sub.ProfileLimit, &sub.Cost)
+		if err != nil {
+			return subscriptions, err
+		}
+
+		subscriptions = append(subscriptions, sub)
+	}
+
+	return subscriptions, err
+}
+
+func (m *memStore) Update(uuid string, subscription Subscription) error {
+	_, err := m.conn.Exec(context.Background(), "UPDATE subscriptions SET name=$1, profile_limit=$2, cost=$3 WHERE uuid=$4", subscription.Name, subscription.ProfileLimit, subscription.Cost, uuid)
+
+	if err == pgx.ErrNoRows {
+		return &NotFoundError{"Subscription not found"}
+	}
+
+	return err
+}
+
+func (m *memStore) Remove(uuid string) error {
+	if _, err := m.conn.Exec(context.Background(), "UPDATE subscriptions SET deleted_at=now() WHERE uuid=$1", uuid); err != nil {
+		if err == pgx.ErrNoRows {
+			return &NotFoundError{"Subscription not found"}
+		}
+		return err
+	}
+
+	return nil
+}
